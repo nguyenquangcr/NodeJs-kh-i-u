d4f4ccfd6207908d8df0b948665a7abf58aae88d
Quang <quang.tran@alta.com.vn>
2022-02-21 17:15:18 +0700

    Update phần cuối khóa mở đầu nodejs

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const yargs = require("yargs");
-// const { addTask } = require("./task");
 const taskFunc = require("./task");
 
 //add
 yargs.command({
   command: "add",
-  build: {
+  builder: {
     title: {
       type: "string",
       demandOption: true,
@@ -16,8 +15,63 @@ yargs.command({
     },
   },
   handler: function (args) {
-    // addTask(args.title, args.description)
     taskFunc.addTask(args.title, args.description);
+  },
+});
+
+//remove task
+yargs.command({
+  command: "remove",
+  builder: {
+    title: {
+      type: "string",
+      demandOption: true,
+    },
+  },
+  handler: function (args) {
+    taskFunc.removeTask(args.title);
+  },
+});
+
+//update task
+yargs.command({
+  command: "update",
+  builder: {
+    title: {
+      type: "string",
+      demandOption: true,
+    },
+    description: {
+      type: "string",
+      demandOption: true,
+    },
+  },
+  handler: function (args) {
+    taskFunc.updateTask(args.title, args.description);
+  },
+});
+
+//list all task
+yargs.command({
+  command: "list",
+  builder: {},
+  handler: function (args) {
+    // taskFunc.removeTask(args.title);
+    taskFunc.listAllTasks();
+  },
+});
+
+//list task detail
+yargs.command({
+  command: "detail",
+  builder: {
+    title: {
+      type: "string",
+      demandOption: true,
+    },
+  },
+  handler: function (args) {
+    taskFunc.detailTasl(args.title);
   },
 });
 
--- task.js
+++ task.js
@@ -11,9 +11,51 @@ const addTask = function (title, description) {
   //add task
   const task = { title, description };
   tasks.push(task);
+  saveTask(tasks);
+};
+
+const updateTask = function (title, description) {
+  //get old tasks
+  const tasks = getTasks();
+  //check exist task
+  // const foundedTask = tasks.find((item) => {
+  //   return item.title == title;
+  // });
+  // if (foundedTask) return;
+  //add task
+  // const task = { title, description };
+  // tasks.push(task);
+  tasks.forEach((item, index) => {
+    if (item.title == title) {
+      tasks[index] = { title: item.title, description: description };
+    }
+  });
+  saveTask(tasks);
+};
+
+const saveTask = function (tasks) {
   const tasksJSON = JSON.stringify(tasks);
-  fs.writeFileSync("tasks.json", tasksJSON);
   //write file
+  fs.writeFileSync("tasks.json", tasksJSON);
+};
+
+const removeTask = function (title) {
+  const tasks = getTasks();
+  const index = tasks.findIndex((item) => {
+    return item.title === title;
+  });
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    saveTask(tasks);
+  }
+};
+
+const listAllTasks = function () {
+  const tasks = getTasks();
+
+  tasks.forEach((item) => {
+    console.log("item", item);
+  });
 };
 
 const getTasks = function () {
@@ -26,6 +68,20 @@ const getTasks = function () {
   }
 };
 
+const detailTasl = function (title) {
+  //get old tasks
+  const tasks = getTasks();
+  //check exist task
+  const foundedTask = tasks.find((item) => {
+    return item.title == title;
+  });
+  console.log("detail: ", foundedTask);
+};
+
 module.exports = {
   addTask,
+  removeTask,
+  listAllTasks,
+  updateTask,
+  detailTasl,
 };
